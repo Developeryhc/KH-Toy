4b4bc31df2c12ec8e47917628cc1469a64e82bbc
SeoJunsik <tjwnstlr3459@naver.com>
2021-06-10 01:09:27 +0900

    예약페이지 예약 및 쿼리문 작성

--- kh_study/src/main/java/kr/or/reservation/model/service/ReservationService.java
+++ kh_study/src/main/java/kr/or/reservation/model/service/ReservationService.java
@@ -4,9 +4,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import kr.or.reservation.model.dao.ReservationDao;
+import kr.or.reservation.model.vo.Reservation;
 
 @Service
 public class ReservationService {
 	@Autowired
 	public ReservationDao dao;
+
+	public int insertReser(Reservation r) {
+		return dao.insertReser(r);
+	}
 }
