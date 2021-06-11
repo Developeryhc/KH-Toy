18600f12c84a3112e49a460d2dafcfd74ae26302
yhch009 <78013461+yhch009@users.noreply.github.com>
2021-06-12 02:23:11 +0900

    20210612_HC reservation 등록 시, record 기록

--- kh_study/src/main/java/kr/or/record/model/service/RecordService.java
+++ kh_study/src/main/java/kr/or/record/model/service/RecordService.java
@@ -2,9 +2,11 @@ package kr.or.record.model.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import kr.or.record.model.dao.RecordDao;
 import kr.or.record.model.vo.Record;
+import kr.or.reservation.model.vo.Reservation;
 
 @Service
 public class RecordService {
@@ -13,6 +15,10 @@ public class RecordService {
 	
 	public Record selectOneRecord(int studentNo) {
 		return dao.selectOneRecord(studentNo);
+	}
+	@Transactional
+	public int insertRecord(Reservation r) {
+		return dao.insertOneRecord(r);
 	}
 
 }
